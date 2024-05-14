7f891605b7df52af77fbd5718cf0d861d62fc777
kamothi <rhkddlf7911@naver.com>
2024-05-14 23:48:54 +0900

    Fix: 기존 토픽의 메시지가 오지 않는 문제 해결

--- loganalize/src/main/java/dis/Main.java
+++ loganalize/src/main/java/dis/Main.java
@@ -15,14 +15,20 @@ public class Main {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("loganalize")
+                .master("spark://192.168.200.156:7077")
                 .getOrCreate();
 
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
         // 카프카 브로커 서버
-        String bootstrapServers = "localhost:9092";
+        String bootstrapServers = "192.168.200.156:9092";
         // 입력 토픽
         String topic = "spark";
+
+        // 시작 오프셋 설정
+        String startingOffsets = "latest"; // 현재 메시지부터 읽기
+
+        String checkpointLocation = "./checkpoint"; // 변경 필요
 
         // 스트리밍 처리를 위한 DataFrame 생성
         Dataset<Row> lines = spark
@@ -30,15 +36,20 @@ public class Main {
                 .format("kafka")
                 .option("kafka.bootstrap.servers", bootstrapServers)
                 .option("subscribe", topic)
-                .option("startingOffsets", "earliest")
+                .option("startingOffsets", startingOffsets) // 시작 오프셋 설정
                 .load();
 
         // 콘솔에 출력하는 예시
         StreamingQuery query = lines
+                .selectExpr("CAST(value AS STRING)")
                 .writeStream()
                 .outputMode("append")
-                .format("console") // 콘솔에 출력
+                .format("csv")
+                .option("path", "./test")  // CSV 파일이 저장될 경로 지정
+                .option("failOnDataLoss", "false")
+                .option("checkpointLocation", checkpointLocation)  // 체크포인트 위치 설정
                 .start();
+
 
         query.awaitTermination();
     }
